c2141edc76314880152364577450760bab6b8bda
Tiefencode <tiefestadt@gmail.com>
2026-03-11 00:26:23 +0100

    feat: binding ui and packaging; new icon

--- scripts/register_driver.bat
+++ scripts/register_driver.bat
@@ -3,7 +3,21 @@ setlocal
 
 set "STEAMVR_ROOT=C:\Program Files (x86)\Steam\steamapps\common\SteamVR"
 set "VRPATHREG=%STEAMVR_ROOT%\bin\win64\vrpathreg.exe"
-set "DRIVER_ROOT=%~dp0"
+set "SCRIPT_DIR=%~dp0"
+set "DRIVER_ROOT=%SCRIPT_DIR%"
+
+if exist "%DRIVER_ROOT%driver.vrdrivermanifest" goto driver_root_ok
+
+set "DRIVER_ROOT=%SCRIPT_DIR%..\build"
+if exist "%DRIVER_ROOT%\driver.vrdrivermanifest" goto driver_root_ok
+
+echo could not locate driver root
+echo checked:
+echo   %SCRIPT_DIR%
+echo   %SCRIPT_DIR%..\build
+exit /b 1
+
+:driver_root_ok
 if "%DRIVER_ROOT:~-1%"=="\" set "DRIVER_ROOT=%DRIVER_ROOT:~0,-1%"
 
 if not exist "%VRPATHREG%" goto :missing_vrpathreg
