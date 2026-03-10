e00a3ab1f0422aafcb4e9f2acbc11d7316ff8e39
Tiefencode <tiefestadt@gmail.com>
2026-03-10 23:10:35 +0100

    fix: wrong driver root in packaged .bat scripts

--- scripts/register_driver.bat
+++ scripts/register_driver.bat
@@ -2,7 +2,7 @@
 setlocal
 set STEAMVR_ROOT=C:\Program Files (x86)\Steam\steamapps\common\SteamVR
 set VRPATHREG=%STEAMVR_ROOT%\bin\win64\vrpathreg.exe
-set DRIVER_ROOT=%~dp0..\build
+set DRIVER_ROOT=%~dp0..
 
 if not exist "%VRPATHREG%" (
   echo vrpathreg.exe not found at %VRPATHREG%
